1390814fba4ecea77b5f03e7b13d2783752f4e3c
Tharvesh Muhaideen A <tharvesh2026@gmail.com>
2025-09-12 09:13:38 +0000

    alter the content to youtbe , insta fb, x, linedin , whatsapp (_for elem

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,6 +1,7 @@
 
 import Link from "next/link";
-import { AppWindow, ArrowRight, Facebook, Twitter, Youtube, Instagram, MessageCircle } from "lucide-react";
+import { AppWindow, ArrowRight, Facebook, Twitter, Youtube, Instagram, Linkedin, MessageCircle } from "lucide-react";
+
 
 const TikTokIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
@@ -59,11 +60,12 @@ export function Footer() {
                 <div className="bg-gray-800 p-4 rounded-lg">
                     <h3 className="font-bold text-white mb-4">Follow us</h3>
                     <div className="flex space-x-4">
-                        <Link href="#" className="hover:text-white transition-colors"><Facebook/></Link>
-                        <Link href="#" className="hover:text-white transition-colors"><Twitter/></Link>
                         <Link href="#" className="hover:text-white transition-colors"><Youtube/></Link>
                         <Link href="#" className="hover:text-white transition-colors"><Instagram/></Link>
-                        <Link href="#" className="hover:text-white transition-colors"><TikTokIcon/></Link>
+                        <Link href="#" className="hover:text-white transition-colors"><Facebook/></Link>
+                        <Link href="#" className="hover:text-white transition-colors"><Twitter/></Link>
+                        <Link href="#" className="hover:text-white transition-colors"><Linkedin/></Link>
+                        <Link href="#" className="hover:text-white transition-colors"><MessageCircle/></Link>
                     </div>
                 </div>
             </div>
