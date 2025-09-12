712a09aa0819c2def32c65882786d6d93b92121f
Tharvesh Muhaideen A <tharvesh2026@gmail.com>
2025-09-12 05:20:44 +0000

    use google's icon for more collections

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -118,19 +118,19 @@ export default function LoginPage() {
          <CardContent>
           <Button variant="outline" className="w-full" onClick={handleGoogleLogin}>
             <svg
-              className="mr-2 h-4 w-4"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="google"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 488 512"
-            >
-              <path
-                fill="currentColor"
-                d="M488 261.8C488 403.3 381.5 512 244 512 111.8 512 0 400.2 0 261.8S111.8 11.6 244 11.6C303.4 11.6 355.8 33.4 394.8 68.4l-64.3 62.1C308.2 110.1 278.3 97.4 244 97.4c-83.8 0-152.3 68.5-152.3 152.9s68.5 152.9 152.3 152.9c98.2 0 130.3-71.1 133.5-108.7H244V261.8h244z"
-              ></path>
+                className="mr-2 h-4 w-4"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="google"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 488 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M488 261.8C488 403.3 381.5 512 244 512 111.8 512 0 400.2 0 261.8S111.8 11.6 244 11.6c60.3 0 115.2 22.1 158.1 58.5l-65.8 63.9C308.2 110.1 278.3 97.4 244 97.4c-83.8 0-152.3 68.5-152.3 152.9s68.5 152.9 152.3 152.9c98.2 0 130.3-71.1 133.5-108.7H244V261.8h244z"
+                ></path>
             </svg>
             Sign in with Google
           </Button>
