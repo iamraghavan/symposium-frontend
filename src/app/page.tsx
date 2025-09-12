2f1da8cbf16fc1f96cde6ce1c0ea1037a64baa3e
Tharvesh Muhaideen A <tharvesh2026@gmail.com>
2025-09-12 10:31:36 +0000

    check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -158,6 +158,7 @@ export default function HomePage() {
                 </Link>
               </div>
             </div>
+           
             <div className="hidden md:block">
                <Image
                   src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
