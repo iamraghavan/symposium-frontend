6a46087d8102c3d76c1ac8f4e13c0fa04d71f8d9
Tharvesh Muhaideen A <tharvesh2026@gmail.com>
2025-09-12 09:07:57 +0000

    remove this entire section (_for element div_)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,6 @@
 
 import Link from "next/link";
 import { AppWindow, ArrowRight, Facebook, Twitter, Youtube, Instagram, MessageCircle } from "lucide-react";
-import { Button } from "../ui/button";
 
 const TikTokIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
@@ -65,13 +64,6 @@ export function Footer() {
                         <Link href="#" className="hover:text-white transition-colors"><Youtube/></Link>
                         <Link href="#" className="hover:text-white transition-colors"><Instagram/></Link>
                         <Link href="#" className="hover:text-white transition-colors"><TikTokIcon/></Link>
-                    </div>
-                </div>
-                 <div className="bg-gray-800 p-4 rounded-lg">
-                    <h3 className="font-bold text-white mb-4">Get the App</h3>
-                     <div className="flex flex-col sm:flex-row gap-4">
-                        <Button variant="ghost" className="bg-gray-700 hover:bg-gray-600 text-white flex-1 justify-center">Download on the Google Play</Button>
-                        <Button variant="ghost" className="bg-gray-700 hover:bg-gray-600 text-white flex-1 justify-center">Download on the App Store</Button>
                     </div>
                 </div>
             </div>
