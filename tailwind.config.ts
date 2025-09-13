d2b0fc2e36e6b6dafd5573f4b46741b375888994
Web EGSPGOI <web@egspec.org>
2025-09-13 02:33:38 +0000

    the fonts are not professional use professional use google sans font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['McLaren', 'cursive'],
-        headline: ['McLaren', 'cursive'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Inter', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
