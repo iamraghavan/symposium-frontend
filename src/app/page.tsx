d29fe51b9f70b329701181259870a9882d8817b3
Tharvesh Muhaideen A <tharvesh2026@gmail.com>
2025-09-22 02:20:51 +0000

    chnage this upcoming event online events and offline event combained sho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -228,7 +228,7 @@ export default function HomePage() {
         className="flex-1">
         <section className="relative h-[80vh] flex items-center justify-center text-center text-white">
           <Image
-            src="https://image-static.collegedunia.com/public/reviewPhotos/871794/IMG-20240914-WA0005.jpg"
+            src="https://cdn.egspec.org/assets/img/conference-hall.webp"
             alt="A vibrant symposium with a diverse audience"
             fill
             className="object-cover -z-10"
